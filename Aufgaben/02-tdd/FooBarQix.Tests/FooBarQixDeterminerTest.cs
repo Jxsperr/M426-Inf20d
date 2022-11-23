c3e6c7ef72a1251fb5d9a21b240dd7e3d1e6cf04
jxsperr <matteo.tschopp@artflorian.ch>
2022-11-23 08:52:39 +0100

    Unittest Test

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,63 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void TestDetermineFoo()
         {
+            // Arrange
+            int a = 6;
+            string expected = "Foo";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDetermineBar()
+        {
+            // Arrange
+            int a = 10;
+            string expected = "Bar";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDetermineQix()
+        {
+            // Arrange
+            int a = 7;
+            string expected = "Qix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDetermineFooBarQix()
+        {
+            // Arrange
+            int a = 210;
+            string expected = "FooBarQix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
