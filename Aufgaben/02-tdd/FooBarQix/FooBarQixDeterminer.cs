9ee36a51cb93e9171aeaecba103044cb5f8d1068
jxsperr <matteo.tschopp@artflorian.ch>
2022-11-23 08:51:25 +0100

    FooBarAufgabe, ohne Tests

--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,15 @@
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string result = "";
 
+            if (input % 3 == 0) result += "Foo";
+            if (input % 5 == 0) result += "Bar";
+            if (input % 7 == 0) result += "Qix";
+
+            return result;
+        }
     }
 }
