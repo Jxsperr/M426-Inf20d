ffcbc29dc90691a9b130661f37aa58c91d301a0f
jxsperr <matteo.tschopp@artflorian.ch>
2022-11-23 08:40:22 +0100

    Aufgabe Unittest Test

--- Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
+++ Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
@@ -6,32 +6,64 @@ namespace Calculator.Tests
     public class CalculatorTest
     {
         [Fact]
-        public void TestAddThreeToTwoPIsFive()
+        public void TestAddTenToSixtyIs70()
         {
             // Arrange
-            int a = 2;
-            int b = 3;
-            int expected = 5;
+            double a = 10;
+            double b = 60;
+            double expected = 70;
             Calculator calculator = new Calculator();
 
-            // Act
-            int actual = calculator.Add(a, b);
+            // When
+            double actual = calculator.Add(a, b);
 
-            // Assert
+            // Then
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void TestSubtractFourFromSixIsTwo()
+        public void TestSubtractTwoFromEightIsSix()
         {
             // Given
-            int a = 6;
-            int b = 4;
-            int expected = 2;
+            double a = 8;
+            double b = 2;
+            double expected = 6;
             Calculator calculator = new Calculator();
 
             // When
-            int actual = calculator.Subtract(a, b);
+            double actual = calculator.Subtract(a, b);
+
+            // Then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMultiplySevenAndNineIsSeventySeven()
+        {
+            // Given
+            double a = 7;
+            double b = 11;
+            double expected = 77;
+            Calculator calculator = new Calculator();
+
+            // When
+            double actual = calculator.Multiply(a, b);
+
+            // Then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDivideOneHundretByTwoIsFifty()
+        {
+            // Given
+            double a = 100;
+            double b = 2;
+            double expected = 50;
+            Calculator calculator = new Calculator();
+
+            // When
+            double actual = calculator.Divide(a, b);
 
             // Then
             Assert.Equal(expected, actual);
